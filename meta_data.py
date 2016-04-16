513d6ff95d4a1947f375dd5eb86c993f6e242d10
ehacinom <ehacinom@gmail.com>
2016-04-15 23:33:38 -0500

    things not working, trying tomorrow

--- meta_data.py
+++ meta_data.py
@@ -4,11 +4,22 @@ import os
 import json
 from collections import defaultdict
 
-def datafiles(filename="./data/datafiles.json", directory="data"):
+def write_json(fn, dictionary):
+    """
+    Just as you think.
     """
     
-    Saves to filename the names of the files in /data, created by
-    get_data.py. Sorted by frequency "A", "Q", "M" (annual/quarter/month).
+    with open(fn, 'wb') as f:
+        json.dump(dictionary, f)
+
+def list_files(filename="./data/datafiles.json", directory="data"):
+    """
+    
+    Saves to filename, in json format, files in a directory, sorted
+    by first letter.
+    
+    Useful for getting assorted data files from get_data.py
+    into groups sorted by frequency.
     
     Note: defaultdict call to deal with unexpected files, like
     - .DS_Store
@@ -16,13 +27,36 @@ def datafiles(filename="./data/datafiles.json", directory="data"):
     
     """
     
+    # search directory for 
     files = defaultdict(list)
     for d in os.listdir(directory):
         files[d[0]].append(directory+"/"+d)
     
-    with open(filename, 'wb') as f:
-        json.dump(files, f)
+    # write to filename
+    write_json(filename, files)
     
+    return files
 
+def aggregate(filename="./data/datafiles.json", 
+              directory="data",
+              out="./data/data.json"):
+    """
+    
+    Aggregate all the files in one file.
+    
+    """
+    
+    data = {"A":{}, "Q": {}, "M": {}}
+    
+    files = list_files(filename, directory)
+    for freq, flist in files.iteritems():
+        if freq not in data: continue
+        for fn in flist:
+            with open(fn, 'r') as f:
+                tag = fn[5:-5]
+                data[freq][tag] = json.load(f)
 
+    # write
+    write_json(out, data)
+    
     
